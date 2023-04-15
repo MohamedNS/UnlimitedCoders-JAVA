0398fcb9cfffec816dd9f942ccc4afc083aed4a0
MohamedNS <mohamednour.soussi@esprit.tn>
2023-04-15 18:07:27 +0100

    remove unused imports

--- src/Gui/HomeConsultationController.java
+++ src/Gui/HomeConsultationController.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;

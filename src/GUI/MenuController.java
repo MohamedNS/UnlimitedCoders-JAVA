094122a72451f8409b509f8d7ba8738bc744a577
MouhebN <mouheb.naddari@esprit.tn>
2023-05-06 10:49:15 +0200

    design fixed + navigation

--- src/GUI/MenuController.java
+++ src/GUI/MenuController.java
@@ -22,122 +22,123 @@ import javafx.stage.Stage;
  */
 public class MenuController implements Initializable {
 
-    public MenuController()
-    {
+    @FXML
+    private Button backID;
+
+    public MenuController() {
     }
 
     @FXML
     private Button btnConsultation;
-	@FXML
-	private Button btnLogout;
+    @FXML
+    private Button btnLogout;
 
     @FXML
     private Button btnMedicament;
-    
+
     @FXML
     private Button btnOrdonnance;
 
-    @FXML
     private Button btnRetour;
 
     @FXML
-    public void btnConsultation(ActionEvent evt)
-    {
-        try{
+    public void btnConsultation(ActionEvent evt) {
+        try {
             System.out.println("Consultation Click");
             Parent loader = FXMLLoader.load(getClass().getResource("HomeConsultation.fxml"));
             Scene scene = new Scene(loader);
             Stage stage = (Stage) btnConsultation.getScene().getWindow();
             stage.setScene(scene);
             stage.show();
-        }
-        catch(IOException ex)
-        {
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
     @FXML
-    public void btnOrdonnance(ActionEvent evt)
-    {
-        try
-        {
+    public void btnOrdonnance(ActionEvent evt) {
+        try {
             System.out.println("Ordonnance Click");
             Parent loader = FXMLLoader.load(getClass().getResource("HomeOrdonnance.fxml"));
             Scene scene = new Scene(loader);
             Stage stage = (Stage) btnOrdonnance.getScene().getWindow();
             stage.setScene(scene);
             stage.show();
-        }
-        catch(IOException ex)
-        {
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
     @FXML
-    public void btnMedicament(ActionEvent evt)
-    {
-        try{
+    public void btnMedicament(ActionEvent evt) {
+        try {
             System.out.println("Medicament Click");
             Parent loader = FXMLLoader.load(getClass().getResource("HomeMedicament.fxml"));
             Scene scene = new Scene(loader);
             Stage stage = (Stage) btnMedicament.getScene().getWindow();
             stage.setScene(scene);
             stage.show();
-        }
-        catch(IOException ex)
-        {
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
-    @FXML
-    public void btnRetour(ActionEvent evt)
-    {
+    public void btnRetour(ActionEvent evt) {
         System.out.println("HomeConsultation fermee");
         Stage stage = (Stage) btnRetour.getScene().getWindow();
         stage.close();
-        
+
     }
-	@FXML
-	public void btnLogout(ActionEvent evt)
-	{
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("../GUI/MenuBienvenu.fxml"));
-		try{
-			loader.load();
-			Parent root = loader.getRoot();
-			Stage stage = (Stage) btnConsultation.getScene().getWindow();
-			Scene scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();
-			}
-		catch(IOException ex)
-		{
-			System.err.println(ex.getMessage());
-		}
-	}
-    
-    public void show()
-    {
-        try{
-            
+
+    @FXML
+    public void btnLogout(ActionEvent evt) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../GUI/BienvenuePatient.fxml"));
+        try {
+            loader.load();
+            Parent root = loader.getRoot();
+            Stage stage = (Stage) btnConsultation.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    public void show() {
+        try {
+
             System.out.println("Consultation Click");
             Parent loader = FXMLLoader.load(getClass().getResource("Menu.fxml"));
             Scene scene = new Scene(loader);
             Stage stage = new Stage();
             stage.setScene(scene);
             stage.show();
-        }
-        catch(IOException ex)
-        {
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-        
+
     }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     }
-    
+
+    @FXML
+    private void BackToMainPage(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../GUI/MenuBienvenu.fxml"));
+        try {
+            loader.load();
+            Parent root = loader.getRoot();
+            Stage stage = (Stage) btnConsultation.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
 }
